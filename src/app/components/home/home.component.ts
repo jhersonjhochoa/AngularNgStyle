2dc34da5ba09869f3d2325b23b6b9f7e5cf3bfda
jhersonjhochoa <jhersonjhochoa@gmail.com>
2020-02-04 20:19:27 -0500

    ciclo de vida

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -21,11 +21,43 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+
+  }
 
   ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked')
+  }
+
+  ngAfterViewInit() {
+    console.log('AfterViewInit')
+  }
+
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked')
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy')
   }
 
 }
--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe( params => {
+      console.log('ruta_padre:', params);
+    });
+  }
 
   ngOnInit() {
   }
